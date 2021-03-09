b8c4f9d4bb31b711c431976496d9c51664463c53
Seth Hill <sethjoshuahill@gmail.com>
2021-03-09 08:57:06 -0700

    I am pretty sure everything is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import axios from 'axios'
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
+import './App.css';
+
+// Base URL https://practiceapi.devmountain.com/api
 
 class App extends Component {
   constructor() {
@@ -19,19 +22,35 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get(`https://practiceapi.devmountain.com/api/posts`)
+     .then(results => {
+      //  console.log(res)
+        this.setState({posts: results.data})
+     })
+     
   }
 
-  updatePost() {
-  
+  updatePost(id,text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(res=> {
+      // console.log(res)
+      this.setState({ posts: res.data })
+    })
+    .catch(err=>console.log(err))
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(res => {
+      this.setState({posts: res.data})
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios(`https://practiceapi.devmountain.com/api/posts`, {text})
+    .then(res => {
+      this.setState({posts: res.data})
+    })
   }
 
   render() {
@@ -43,7 +62,18 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn= {this.createPost}/>
+
+            {
+              posts.map( post => (
+                <Post key={post.id}  
+                text={post.text} 
+                date={post.date} 
+                id={post.id} 
+                updatePostFn={this.updatePost} 
+                deletePostFn={this.deletePost}/>
+              ))
+            }
           
         </section>
       </div>
